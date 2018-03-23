2d0abd1892e2fd253b76dcd560170edcc7311adc
Sino <ilyas.alfan.b@gmail.com>
2018-03-23 10:14:08 +0100

    Reading from CSV files

--- src/exercise1.R
+++ src/exercise1.R
@@ -1,6 +1,31 @@
-workingDir <- getwd()
+# Reads a CSV file from the 'src/csv/' directory.
+getDataFromCSV <- function(fileName) {
+  workingDir <- getwd()
+  
+  csvDirectory <- "/src/csv/"
+  fullPath <- paste(workingDir, csvDirectory, fileName, sep = "")
+  
+  return(read.csv(file = fullPath))
+}
 
-fileName <- "csv/wine_quality.csv"
-fullPath <- paste(workingDir, fileName, sep = "")
+# Reads the 'house_wine_red.csv' file from the 'src/csv/' directory.
+csvGetHouseWineRed <- function() {
+  return(getDataFromCSV(fileName = "house_wine_red.csv"))
+}
 
-wineQuality <- read.csv(file = fullPath)
+# Reads the 'house_wine_white.csv' file from the 'src/csv/' directory.
+csvGetHouseWineWhite <- function() {
+  return(getDataFromCSV(fileName = "house_wine_white.csv"))
+}
+
+# Reads the 'wine_quality.csv' file from the 'src/csv/' directory.
+csvGetWineQuality <- function() {
+  return(getDataFromCSV(fileName = "wine_quality.csv"))
+}
+
+# Attempts to fetch the residual sugar data from the given data set,
+# generate a boxplot for it and return it May throw An exception if
+# no such variable is found.
+generateResidualSugarBoxPlot <- function(data) {
+  return(boxplot(data$residual.sugar))
+}
